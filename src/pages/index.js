7fc488f3649234bc6ecc369a74a50e99099f6ec2
emonibnsalim <emonibnsali@gmail.com>
2023-12-19 01:54:13 +0600

    practice ssg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
 
 
-const HomePage = () => {
+const HomePage = ({allNewsData}) => {
+  console.log(allNewsData)
   return (
     <>
       <Head>
@@ -16,6 +18,7 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
+      <AllNews allNewsData={allNewsData} />
     </>
   );
 };
@@ -24,3 +27,16 @@ export default HomePage;
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news")
+  const data = await res.json();
+  return {
+    props: {
+      allNewsData: data,
+    },
+    revalidate: 10,
+  }
+
+}
